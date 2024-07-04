6966b5afc13a734fa5f7342557bc71e415166280
igor subbotin <subbotin@individ.ru>
2024-07-04 08:02:03 +0300

    stage_7

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,16 +1,16 @@
 <script setup>
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import NavItem from './NavItem.vue'
+
 const navItems = { timeline: ClockIcon, activities: ListBulletIcon, progress: ChartBarIcon }
 </script>
 
 <template>
   <nav class="sticky bottom-0 z-20 bg-white border-t">
     <ul class="flex items-center justify-around">
-      <li v-for="(icon, page) in navItems" :key="page" class="flex-1">
-        <a :href="`#${page}`" class="flex flex-col p-2 items-center text-xs capitalize">
-          <component :is="icon" class="h-6 w-6" /> {{ page }}
-        </a>
-      </li>
+      <NavItem v-for="(icon, page) in navItems" :key="page" :href="`#${page}`">
+        <component :is="icon" class="h-6 w-6" /> {{ page }}
+      </NavItem>
     </ul>
   </nav>
 </template>
